24de4c7afcd4aea1f69ff32a773376b7851efac2
Ankit Singh <monusingh928469@gmail.com>
2023-08-28 13:47:55 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
 
 
+app.use(express.urlencoded({ extended: true }));
 
 
+app.use(express.static('./assets'));
 
+app.use(expressLayouts);
+//Set Up - Extract Styles from Sub Pages into the Layout.
+app.set("layout extractStyles", true);
+//Set Up - Extract Scripts from Sub Pages into the Layout.
+app.set("layout extractScripts", true);
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
+app.use('/', require('./routes/index'));
 
 
 app.listen(port, (error)=> {
