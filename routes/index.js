11c4d4e969dc3937455aacf26e35d8b8bd2e072e
Ankit Singh <monusingh928469@gmail.com>
2023-08-28 16:49:34 +0530

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
-const express = require('express');
+//Require the existing Express
+const express = require("express");
+//Create an Index Router
 const router = express.Router();
 
-const homeController = require('../controllers/home_controller');
+//Require the Home Router
+const homeRouter = require("./home");
+//Require the Jobs Router
+const jobsRouter = require("./jobs");
+//Require the Downloads Router
+const downloadsRouter = require("./downloads");
+//Require the Students Router
+const studentsRouter = require("./students");
+//Require the Interviews Router
+const interviewsRouter = require("./interviews");
 
-router.get('/', homeController.home);
+//Use the Home Router
+router.use("/", homeRouter);
+//Use the Jobs Router
+router.use("/jobs", jobsRouter);
+//Use the Downloads Router
+router.use("/report", downloadsRouter);
+//Use the Students Router
+router.use("/students", studentsRouter);
+//Use the Interviews Router
+router.use("/interviews", interviewsRouter);
 
-
-
+//Export the Index Router
 module.exports = router;
